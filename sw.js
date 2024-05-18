1be0a44e83a56baa92a8262e1039fca86cd26564
Jakub Jankiewicz <jcubic@onet.pl>
2024-05-18 18:38:30 +0200

    add redirect

--- sw.js
+++ sw.js
@@ -5,3 +5,7 @@ const app = new Wayne();
 app.get('/api/hello/', (req, res) => {
    res.text('Hello, Service Worker!');
 });
+
+app.get('/api/hello', (req, res) => {
+   res.redirect(301, req.url + '/');
+});
