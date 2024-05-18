bee7b10f2b7617a983d13fbec3cb36be1d2808d7
Jakub Jankiewicz <jcubic@onet.pl>
2024-05-18 21:15:09 +0200

    replace double quote with single quote

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { join } from "node:path";
-import { buildSync } from "esbuild";
+import { join } from 'node:path';
+import { buildSync } from 'esbuild';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     {
-      apply: "build",
-      enforce: "post",
+      apply: 'build',
+      enforce: 'post',
       transformIndexHtml() {
         buildSync({
           minify: true,
           bundle: true,
-          entryPoints: [join(process.cwd(), "sw.js")],
-          outfile: join(process.cwd(), "dist", "sw.js"),
+          entryPoints: [join(process.cwd(), 'src', 'sw.js')],
+          outfile: join(process.cwd(), 'dist', 'sw.js')
         });
-      },
-    },
-  ],
+      }
+    }
+  ]
 });
